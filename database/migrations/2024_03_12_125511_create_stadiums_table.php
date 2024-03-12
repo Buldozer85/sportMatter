b4854e47db3e712b5b9035aab67d016a3929f083
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2024-03-12 13:44:46 +0100

    basic migrations

--- database/migrations/2024_03_12_125511_create_stadiums_table.php
+++ database/migrations/2024_03_12_125511_create_stadiums_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('sports', function (Blueprint $table) {
+        Schema::create('stadiums', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('capacity');
             $table->string('name');
+
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::drop('sports');
+        Schema::dropIfExists('stadiums');
     }
 };
