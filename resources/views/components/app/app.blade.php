522ea54c253d56cf101a1a414df81031c8d19d71
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2024-03-14 13:27:44 +0100

    registrace a login link

--- resources/views/components/app/app.blade.php
+++ resources/views/components/app/app.blade.php
@@ -70,15 +70,18 @@
                         -->
                         <div x-show="openProfile" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Profil</a>
+                            <a href="{{ route('app.logout') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Odhlásit se</a>
                         </div>
                     </div>
                         @endauth
 
                     @guest
-                        <a>Přihlásit se</a>
+                        <div class="px-4">
+                            <a href="{{ route('app.show-login') }}">Přihlásit se</a>
+                            <a href="{{ route('app.show-register') }}">Registrace</a>
+                        </div>
+
                     @endguest
                 </div>
                 <div class="-mr-2 flex items-center sm:hidden">
