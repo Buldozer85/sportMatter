4e40d61eca701e2cadc4ca9eac098e5962a58f48
Tomáš Dvořák <tomas.dvorak@geekcoders.cz>
2024-04-01 15:44:17 +0200

    template měnících se inoutů podle selectu

--- resources/views/admin/games/update.blade.php
+++ resources/views/admin/games/update.blade.php
@@ -1,11 +1,13 @@
 <x-admin::layouts.dashboard title="Vytvoření zápasu">
     <form method="post">
         <h1 class="text-center font-bold text-xl">Vytvoření zápasu</h1>
-        <div class="flex flex-col mb-4 mt-5">
+        <div class="flex flex-col mb-4 mt-5" x-data="{
+            selectedSport: {{ old('sport') ?? 1}}
+        }">
             <div class="flex flex-row gap-5">
                 <div class="w-6/12">
                     <x-admin.forms.select
-                        label="Domácí tým"
+                        label="Domácí tým/hráč"  {{--         TODO: měnit podle selected sportu, výsledky zapisovat v params           --}}
                         id="home_team"
                         name="home_team"
                         :options="[
@@ -15,7 +17,7 @@
                 </div>
                 <div class="w-6/12">
                     <x-admin.forms.select
-                        label="Venkovní tým"
+                        label="Venkovní tým/hráč"
                         id="away_team"
                         name="away_team"
                         :options="[
@@ -52,6 +54,33 @@
                     1 => 'Jan Krupička',
                   ]"
                     />
+                </div>
+
+                <div class="w-6/12">
+                    <x-admin.forms.select
+                        x-model="selectedSport"
+                        label="Sport"
+                        id="sport"
+                        name="sport"
+                        :options="[
+                    1 => 'Šipky',
+                    2 => 'Fotbal',
+                    3 => 'Hokej'
+                  ]"
+                    />
+                </div>
+            </div>
+            <h2>Parametry</h2>
+            <div>
+                <div x-show="selectedSport == 1">   {{--         TODO: Jestli neudělat kontrolu spíš podle nějakého klíče idk           --}}
+                    <x-admin.forms.input name="best_of" id="best_of" label="Nejlepší z x"/>  {{--         Myšlwno jako text input, bude se tam zadávat např. z 5 legů nebo 3 setů           --}}
+                    <x-admin.forms.input name="count_of_legs_first_player"/> {{--        TODO: same pro druhého hráče, ještě i sety           --}}
+                   <x-admin.forms.input name="avarage_player_one"></x-admin.forms.input> {{--        TODO: same pro druhého hráče          --}}
+                </div>
+
+                <div x-show="selectedSport == 2">   {{--         TODO: Jestli neudělat kontrolu spíš podle nějakého klíče idk           --}}
+                    <x-admin.forms.input name="" id="best_of" label="Nejlepší z x"/>  {{--         Myšlwno jako text input, bude se tam zadávat např. z 5 legů nebo 3 setů           --}}
+
                 </div>
             </div>
             <div class="flex justify-center mt-5 ">
